034ec7df067e46ed40aa82d8aaa1557def5d5218
domenvajevec <domenvajevec@gmail.com>
2014-02-26 09:20:49 -0800

    ids

--- config/initializers/instagram.rb
+++ config/initializers/instagram.rb
@@ -1,8 +1,8 @@
 require "instagram"
 
 Instagram.configure do |config|
-  config.client_id = ENV['INSTAGRAM_CLIENT_ID']
-  config.client_secret = ENV['INSTAGRAM_CLIENT_SECRET']
+  config.client_id = '81becf58c5ca4d06bfcfa366fa652228'
+  config.client_secret = '6a06465bd1d04416816e976402eb8230'
 end
 
 CALLBACK_URL = "http://lit-earth-1048.herokuapp.com/session/callback"
